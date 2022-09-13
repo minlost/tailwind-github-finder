e2542d460ff3ef57aaa043c14d4fb0a96db0c31f
minlost <97989349+minlost@users.noreply.github.com>
2022-09-13 21:59:32 +0200

    first commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { FaGithub } from 'react-icons/fa';
+
+function Navbar({ title }) {
+  return (
+    <nav className="mb-12 shadow-lg navbar bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          <Link className="text-lg font-bold align-middle" to='/'>{title}</Link>
+        </div> 
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link className="btn btn-ghost btn-sm rounded-btn" to='/'>Home</Link> 
+            <Link className="btn btn-ghost btn-sm rounded-btn" to='/about'>About</Link>
+          </div>
+        </div>
+
+       
+   
+      </div>
+    </nav>
+  );
+}
+
+Navbar.defaultProps = {
+  title: 'GitHub Finder'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default Navbar;
